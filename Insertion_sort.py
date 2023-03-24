b02842ff4bbeaf6d2aac64d1f86cef72b70b8c27
Anirban Mahapatra <121116611+anirban-mahapatra@users.noreply.github.com>
2023-03-24 15:13:33 +0530

    Update Insertion_sort.py

--- Insertion_sort.py
+++ Insertion_sort.py
@@ -1,15 +1,5 @@
 """Problem:- Write a python program for Insertion sort using function."""
 
-# def sort(arr,no):
-#     for i in range(1,no):
-#         key=arr[i]
-#         j=i-1
-#         while(j>=0 and arr[j]>key):
-#             arr[j+1]=arr[j]
-#             j-=1
-#         arr[j+1]=key
-
-
 def sort(arr,no):
     for i in range(1,no):
         j=i-1
